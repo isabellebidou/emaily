d267a58b536dd2a767a599884b94bfc9e0b259a0
isabelle <isa.bidou@gmail.com>
2022-11-11 21:21:02 +0100

    github commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,11 @@ import { legacy_createStore ,applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios'
+window.axios= axios;
+// in browser console
+//const survey = {title: 'essay survey', subject: 'give us feedback', recipients: 'isa.bidou@gmail.com', body: 'did you like our essay?'}
+//axios.post('/api/surveys', survey)
 const store  = legacy_createStore(reducers,{},applyMiddleware(reduxThunk));
 
 ReactDOM.render(
